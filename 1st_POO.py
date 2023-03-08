9d77de653d7eaa8af3b33840ab94a49b5c3eb9dc
YannSko <101813005+YannSko@users.noreply.github.com>
2023-03-08 20:58:45 +0100

    Create 1st_POO.py

---
+++ 1st_POO.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+my_dog = Dog("Fido", "Labrador")
+
+my_dog.bark()
